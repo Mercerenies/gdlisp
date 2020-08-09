7f346b3f9d0784de175bc3288ca236f1e2100b5c
Silvio Mayolo <mercerenies@comcast.net>
2020-08-09 17:38:33 -0400

    Dotted list tests

--- src/sxp/dotted.rs
+++ src/sxp/dotted.rs
@@ -45,4 +45,48 @@ impl<'a> TryFrom<DottedExpr<'a>> for Vec<&'a AST> {
 
 }
 
-//// Tests
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::sxp::ast;
+
+  #[test]
+  fn simple_dot() {
+    let ast = AST::Int(33);
+    let dot = DottedExpr::new(&ast);
+
+    assert!(dot.elements.is_empty());
+    assert_eq!(*dot.terminal, ast);
+    assert!(Vec::try_from(dot).is_err());
+
+  }
+
+  #[test]
+  fn proper_list() {
+    let vec = vec!(AST::Int(1), AST::Int(2), AST::Int(3));
+    let ast = ast::list(vec.clone());
+    let dot = DottedExpr::new(&ast);
+
+    assert_eq!(dot.elements, vec.iter().collect::<Vec<_>>());
+    assert_eq!(*dot.terminal, AST::Nil);
+
+    let vec1 = Vec::try_from(dot);
+    assert_eq!(vec1, Ok(vec.iter().collect()));
+
+  }
+
+  #[test]
+  fn improper_list() {
+    let vec = vec!(AST::Int(1), AST::Int(2), AST::Int(3));
+    let end = AST::Int(4);
+    let ast = ast::dotted_list(vec.clone(), end.clone());
+    let dot = DottedExpr::new(&ast);
+
+    assert_eq!(dot.elements, vec.iter().collect::<Vec<_>>());
+    assert_eq!(*dot.terminal, end);
+
+    assert!(Vec::try_from(dot).is_err());
+
+  }
+
+}
