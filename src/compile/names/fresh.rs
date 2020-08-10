396483db0e7694b6cbd27b0c5ab2a73f72000a03
Silvio Mayolo <mercerenies@comcast.net>
2020-08-10 14:33:39 -0400

    Fresh name generator no longer stores its prefix

--- src/compile/names/fresh.rs
+++ src/compile/names/fresh.rs
@@ -1,32 +1,28 @@
 
 pub struct FreshNameGenerator<'a> {
   reserved: Vec<&'a str>,
-  prefix: String,
   index: u32,
 }
 
 impl<'a> FreshNameGenerator<'a> {
 
-  pub fn default_prefix() -> String {
-    String::from("_G")
-  }
+  pub const DEFAULT_PREFIX: &'static str = "_G";
 
-  pub fn with_prefix(reserved: Vec<&'a str>, prefix: String) -> FreshNameGenerator<'a> {
+  pub fn new(reserved: Vec<&'a str>) -> FreshNameGenerator<'a> {
     FreshNameGenerator {
       reserved: reserved,
-      prefix: prefix,
       index: 0,
     }
   }
 
-  pub fn new(reserved: Vec<&'a str>) -> FreshNameGenerator<'a> {
-    FreshNameGenerator::with_prefix(reserved, FreshNameGenerator::default_prefix())
+  pub fn generate(&mut self) -> String {
+    self.generate_with(FreshNameGenerator::DEFAULT_PREFIX)
   }
 
-  pub fn generate(&mut self) -> String {
+  pub fn generate_with(&mut self, prefix: &str) -> String {
     let mut name: String;
     loop {
-      name = format!("{}{}", self.prefix, self.index);
+      name = format!("{}_{}", prefix, self.index);
       self.index += 1;
       if !self.reserved.contains(&&*name) {
         break;
@@ -44,33 +40,33 @@ mod tests {
   #[test]
   fn empty_generate() {
     let mut gen = FreshNameGenerator::new(vec!());
-    assert_eq!(gen.generate(), "_G0");
-    assert_eq!(gen.generate(), "_G1");
-    assert_eq!(gen.generate(), "_G2");
+    assert_eq!(gen.generate(), "_G_0");
+    assert_eq!(gen.generate(), "_G_1");
+    assert_eq!(gen.generate(), "_G_2");
   }
 
   #[test]
   fn unrelated_generate() {
     let mut gen = FreshNameGenerator::new(vec!("foo", "bar", "these_names_change_nothing", "a99"));
-    assert_eq!(gen.generate(), "_G0");
-    assert_eq!(gen.generate(), "_G1");
-    assert_eq!(gen.generate(), "_G2");
+    assert_eq!(gen.generate(), "_G_0");
+    assert_eq!(gen.generate(), "_G_1");
+    assert_eq!(gen.generate(), "_G_2");
   }
 
   #[test]
   fn conflicting_generate() {
-    let mut gen = FreshNameGenerator::new(vec!("_G1", "_G3"));
-    assert_eq!(gen.generate(), "_G0");
-    assert_eq!(gen.generate(), "_G2");
-    assert_eq!(gen.generate(), "_G4");
+    let mut gen = FreshNameGenerator::new(vec!("_G_1", "_G_3"));
+    assert_eq!(gen.generate(), "_G_0");
+    assert_eq!(gen.generate(), "_G_2");
+    assert_eq!(gen.generate(), "_G_4");
   }
 
   #[test]
   fn custom_prefix() {
-    let mut gen = FreshNameGenerator::with_prefix(vec!("foo1", "foo3", "_G0"), String::from("foo"));
-    assert_eq!(gen.generate(), "foo0");
-    assert_eq!(gen.generate(), "foo2");
-    assert_eq!(gen.generate(), "foo4");
+    let mut gen = FreshNameGenerator::new(vec!("foo_1", "foo_3", "_G_0"));
+    assert_eq!(gen.generate_with("foo"), "foo_0");
+    assert_eq!(gen.generate_with("foo"), "foo_2");
+    assert_eq!(gen.generate_with("foo"), "foo_4");
   }
 
 }
