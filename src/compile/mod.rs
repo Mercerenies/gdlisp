0e83265506765babb572e6dcfe8ad3fc44967a41
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-10-06 13:22:56 -0400

    __is_gdlisp_function

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -263,6 +263,11 @@ impl<'a> Compiler<'a> {
     for var in closed_vars {
       class_body.push(Decl::VarDecl(var.clone(), None));
     }
+    // TODO Actual true in GDScript AST
+    // TODO Put these field names somewhere central and reserve them in the name generator
+    class_body.push(Decl::VarDecl(String::from("__is_gdlisp_function"),
+                                  Some(Expr::Var(String::from("true")))));
+    /////
     class_body.append(&mut vec!(
       Decl::FnDecl(decl::Static::NonStatic, constructor),
       Decl::FnDecl(decl::Static::NonStatic, func),
