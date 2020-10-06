17dca927d77c1fd9b865e3de1c3d6ae1c1ba5203
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-10-06 12:50:57 -0400

    Test cases

--- src/compile/symbol_table/function_call.rs
+++ src/compile/symbol_table/function_call.rs
@@ -77,3 +77,5 @@ impl FnSpecs {
   }
 
 }
+
+// TODO Tests
--- src/ir/arglist.rs
+++ src/ir/arglist.rs
@@ -148,3 +148,69 @@ impl From<ArgList> for FnSpecs {
   }
 
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::parser;
+  use crate::sxp::dotted::DottedExpr;
+
+  fn parse_ast(input: &str) -> AST {
+    let parser = parser::ASTParser::new();
+    parser.parse(input).unwrap()
+  }
+
+  fn parse_arglist(input: &str) -> Result<ArgList, ArgListParseError> {
+    let ast = parse_ast(input);
+    let dotted: Vec<_> = DottedExpr::new(&ast).try_into().unwrap();
+    ArgList::parse(dotted)
+  }
+
+  fn arglist(req: Vec<&str>, opt: Vec<&str>, rest: Option<&str>) -> ArgList {
+    ArgList {
+      required_args: req.into_iter().map(|x| x.to_owned()).collect(),
+      optional_args: opt.into_iter().map(|x| x.to_owned()).collect(),
+      rest_arg: rest.map(|x| x.to_owned()),
+    }
+  }
+
+  fn gdarglist(req: Vec<&str>) -> GDArgList {
+    GDArgList::required(req.into_iter().map(|x| x.to_owned()).collect())
+  }
+
+  fn into_gd(args: ArgList) -> (GDArgList, Vec<(String, String)>) {
+    let mut tmp = FreshNameGenerator::new(vec!());
+    args.into_gd_arglist(&mut tmp)
+  }
+
+  #[test]
+  fn test_parsing() {
+    assert_eq!(parse_arglist("()").unwrap(), arglist(vec!(), vec!(), None));
+    assert_eq!(parse_arglist("(a)").unwrap(), arglist(vec!("a"), vec!(), None));
+    assert_eq!(parse_arglist("(a b)").unwrap(), arglist(vec!("a", "b"), vec!(), None));
+    assert_eq!(parse_arglist("(a b &opt c)").unwrap(), arglist(vec!("a", "b"), vec!("c"), None));
+    assert_eq!(parse_arglist("(a &rest rest)").unwrap(), arglist(vec!("a"), vec!(), Some("rest")));
+    assert_eq!(parse_arglist("(a b c &opt d &rest e)").unwrap(), arglist(vec!("a", "b", "c"), vec!("d"), Some("e")));
+    assert_eq!(parse_arglist("(a b c &opt d e &rest f)").unwrap(), arglist(vec!("a", "b", "c"), vec!("d", "e"), Some("f")));
+    assert_eq!(parse_arglist("(a b c &opt d e)").unwrap(), arglist(vec!("a", "b", "c"), vec!("d", "e"), None));
+  }
+
+  #[test]
+  fn test_invalid_parse() {
+    assert!(parse_arglist("(&silly-name)").is_err());
+    assert!(parse_arglist("(&opt a &opt b)").is_err());
+    assert!(parse_arglist("(&rest a &opt b)").is_err());
+    assert!(parse_arglist("(&rest a &rest b)").is_err());
+    assert!(parse_arglist("(&rest a b)").is_err());
+  }
+
+  #[test]
+  fn test_arglist_gen() {
+    assert_eq!(into_gd(arglist(vec!(), vec!(), None)).0, gdarglist(vec!()));
+    assert_eq!(into_gd(arglist(vec!("a"), vec!(), None)).0, gdarglist(vec!("a_0")));
+    assert_eq!(into_gd(arglist(vec!("a", "b"), vec!(), None)).0, gdarglist(vec!("a_0", "b_1")));
+    assert_eq!(into_gd(arglist(vec!("a"), vec!("b"), None)).0, gdarglist(vec!("a_0", "b_1")));
+    assert_eq!(into_gd(arglist(vec!("a"), vec!("b"), Some("r"))).0, gdarglist(vec!("a_0", "b_1", "r_2")));
+  }
+
+}
