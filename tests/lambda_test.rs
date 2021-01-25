a8b892cb0e4fafeb94ec305e8b54d0c8465ee078
Silvio Mayolo <mercerenies@comcast.net>
2021-01-24 21:30:08 -0500

    Additional test

--- tests/lambda_test.rs
+++ tests/lambda_test.rs
@@ -9,3 +9,15 @@ fn simple_lambda_test() {
   let output = parse_and_run("((print (funcall (lambda (a) a) 1)))");
   assert_eq!(output, "\n1\n");
 }
+
+#[test]
+#[ignore]
+fn closure_lambda_test() {
+  let output = parse_and_run(r#"
+  ((let ((a (let ((x 99))
+              (let ((f (lambda (z) x)))
+                f))))
+    (print (funcall a ()))))
+  "#);
+  assert_eq!(output, "\n99\n");
+}
