adc69caf46917a5a867ef91c869a3cecde3340d3
Silvio Mayolo <mercerenies@comcast.net>
2021-02-11 20:10:32 -0500

    Some helpers to make symbol tables iterate in the order of insertion

--- src/ir/symbol_table.rs
+++ src/ir/symbol_table.rs
@@ -5,7 +5,8 @@ use std::collections::HashMap;
 
 #[derive(Clone, Debug, Default)]
 pub struct SymbolTable {
-  functions: HashMap<String, Decl>,
+  functions: HashMap<String, usize>,
+  in_order: Vec<Decl>,
 }
 
 impl SymbolTable {
@@ -15,11 +16,12 @@ impl SymbolTable {
   }
 
   pub fn get(&self, name: &str) -> Option<&Decl> {
-    self.functions.get(name)
+    self.functions.get(name).map(|idx| &self.in_order[*idx])
   }
 
   pub fn set(&mut self, name: String, value: Decl) {
-    self.functions.insert(name, value);
+    self.functions.insert(name, self.in_order.len());
+    self.in_order.push(value);
   }
 
   pub fn del(&mut self, name: &str) {
@@ -28,6 +30,14 @@ impl SymbolTable {
 
   pub fn has(&self, name: &str) -> bool {
     self.get(name).is_some()
+  }
+
+}
+
+impl From<SymbolTable> for Vec<Decl> {
+
+  fn from(table: SymbolTable) -> Vec<Decl> {
+    table.in_order
   }
 
 }
