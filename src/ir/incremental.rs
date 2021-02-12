7d6360181572ea40a02acf77dcb56be3e65f0e3b
Silvio Mayolo <mercerenies@comcast.net>
2021-02-11 20:10:44 -0500

    Moved a lot of decl IR stuff to IncCompiler

--- src/ir/incremental.rs
+++ src/ir/incremental.rs
@@ -2,6 +2,17 @@
 // Incremental compilation (supplies backbone for macro resolution)
 
 use super::symbol_table::SymbolTable;
+use super::{MAIN_BODY_NAME, resolve_call_name, resolve_special_form};
+use super::arglist::ArgList;
+use super::literal::Literal;
+use super::expr::Expr;
+use super::decl::{self, Decl};
+use crate::sxp::dotted::DottedExpr;
+use crate::sxp::ast::AST;
+use crate::compile::error::Error;
+
+use std::convert::TryInto;
+use std::borrow::Borrow;
 
 pub struct IncCompiler {
   symbols: SymbolTable,
@@ -13,12 +24,133 @@ impl IncCompiler {
     IncCompiler { symbols: SymbolTable::new() }
   }
 
+  pub fn compile_expr(&self, expr: &AST) -> Result<Expr, Error> {
+    match expr {
+      AST::Nil | AST::Cons(_, _) => {
+        let vec: Vec<&AST> = DottedExpr::new(expr).try_into()?;
+        if vec.is_empty() {
+          Ok(Expr::Literal(Literal::Nil))
+        } else {
+          let head = resolve_call_name(vec[0])?;
+          let tail = &vec[1..];
+          if let Some(sf) = resolve_special_form(head, tail)? {
+            Ok(sf)
+          } else {
+            let args = tail.iter().map(|x| self.compile_expr(x)).collect::<Result<Vec<_>, _>>()?;
+            Ok(Expr::Call(head.to_owned(), args))
+          }
+        }
+      }
+      AST::Array(vec) => {
+        let vec = vec.iter().map(|e| self.compile_expr(e)).collect::<Result<Vec<_>, _>>()?;
+        Ok(Expr::Array(vec))
+      }
+      AST::Int(n) => {
+        Ok(Expr::Literal(Literal::Int(*n)))
+      }
+      AST::Bool(b) => {
+        Ok(Expr::Literal(Literal::Bool(*b)))
+      }
+      AST::Float(f) => {
+        Ok(Expr::Literal(Literal::Float(*f)))
+      }
+      AST::String(s) => {
+        Ok(Expr::Literal(Literal::String(s.to_owned())))
+      }
+      AST::Symbol(s) => {
+        Ok(Expr::LocalVar(s.to_string()))
+      }
+    }
+  }
+
+  pub fn compile_decl(&self, decl: &AST)
+                      -> Result<Decl, Error> {
+    let vec: Vec<&AST> = DottedExpr::new(decl).try_into()?;
+    if vec.is_empty() {
+      return Err(Error::InvalidDecl(decl.clone()));
+    }
+    match vec[0] {
+      AST::Symbol(s) => {
+        match s.borrow() {
+          "defn" => {
+            if vec.len() < 3 {
+              return Err(Error::InvalidDecl(decl.clone()));
+            }
+            let name = match vec[1] {
+            AST::Symbol(s) => s,
+              _ => return Err(Error::InvalidDecl(decl.clone())),
+            };
+            let args: Vec<_> = DottedExpr::new(vec[2]).try_into()?;
+            let args = ArgList::parse(args)?;
+            let body = vec[3..].iter().map(|expr| self.compile_expr(expr)).collect::<Result<Vec<_>, _>>()?;
+            Ok(Decl::FnDecl(decl::FnDecl {
+              name: name.to_owned(),
+              args: args,
+              body: Expr::Progn(body),
+            }))
+          }
+          "defmacro" => {
+            if vec.len() < 3 {
+              return Err(Error::InvalidDecl(decl.clone()));
+            }
+            let name = match vec[1] {
+              AST::Symbol(s) => s,
+              _ => return Err(Error::InvalidDecl(decl.clone())),
+            };
+            let args: Vec<_> = DottedExpr::new(vec[2]).try_into()?;
+            let args = ArgList::parse(args)?;
+            let body = vec[3..].iter().map(|expr| self.compile_expr(expr)).collect::<Result<Vec<_>, _>>()?;
+            Ok(Decl::MacroDecl(decl::MacroDecl {
+              name: name.to_owned(),
+              args: args,
+              body: Expr::Progn(body),
+            }))
+          }
+          _ => {
+            Err(Error::UnknownDecl(s.clone()))
+          }
+        }
+      }
+      _ => {
+        Err(Error::InvalidDecl(decl.clone()))
+      }
+    }
+  }
+
+  pub fn compile_toplevel(mut self, body: &AST)
+                          -> Result<Vec<Decl>, Error> {
+    let body: Vec<_> = DottedExpr::new(body).try_into()?;
+    let mut main: Vec<Expr> = Vec::new();
+    for curr in body {
+      match self.compile_decl(curr) {
+        Ok(d) => self.symbols.set(d.name().to_owned(), d),
+        Err(Error::UnknownDecl(_)) => main.push(self.compile_expr(curr)?),
+        Err(e) => return Err(e),
+      }
+    }
+    let main_decl = Decl::FnDecl(decl::FnDecl {
+      name: MAIN_BODY_NAME.to_owned(),
+      args: ArgList::empty(),
+      body: Expr::Progn(main),
+    });
+    self.symbols.set(MAIN_BODY_NAME.to_owned(), main_decl);
+    Ok(self.into())
+  }
+
 }
 
 impl Default for IncCompiler {
 
   fn default() -> IncCompiler {
     IncCompiler::new()
+  }
+
+}
+
+impl From<IncCompiler> for Vec<Decl> {
+
+  fn from(compiler: IncCompiler) -> Vec<Decl> {
+    compiler.symbols.into()
   }
 
 }
--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -19,130 +19,25 @@ pub mod incremental;
 
 use expr::Expr;
 use decl::Decl;
-use arglist::ArgList;
-use literal::Literal;
 use crate::sxp::ast::AST;
-use crate::sxp::dotted::DottedExpr;
 use crate::compile::error::Error;
-
-use std::convert::TryInto;
-use std::borrow::Borrow;
 
 pub const MAIN_BODY_NAME: &str = "run";
 
 pub fn compile_expr(expr: &AST)
                     -> Result<Expr, Error> {
-  match expr {
-    AST::Nil | AST::Cons(_, _) => {
-      let vec: Vec<&AST> = DottedExpr::new(expr).try_into()?;
-      if vec.is_empty() {
-        Ok(Expr::Literal(Literal::Nil))
-      } else {
-        let head = resolve_call_name(vec[0])?;
-        let tail = &vec[1..];
-        if let Some(sf) = resolve_special_form(head, tail)? {
-          Ok(sf)
-        } else {
-          let args = tail.iter().map(|x| compile_expr(x)).collect::<Result<Vec<_>, _>>()?;
-          Ok(Expr::Call(head.to_owned(), args))
-        }
-      }
-    }
-    AST::Array(vec) => {
-      let vec = vec.iter().map(compile_expr).collect::<Result<Vec<_>, _>>()?;
-      Ok(Expr::Array(vec))
-    }
-    AST::Int(n) => {
-      Ok(Expr::Literal(Literal::Int(*n)))
-    }
-    AST::Bool(b) => {
-      Ok(Expr::Literal(Literal::Bool(*b)))
-    }
-    AST::Float(f) => {
-      Ok(Expr::Literal(Literal::Float(*f)))
-    }
-    AST::String(s) => {
-      Ok(Expr::Literal(Literal::String(s.to_owned())))
-    }
-    AST::Symbol(s) => {
-      Ok(Expr::LocalVar(s.to_string()))
-    }
-  }
+  incremental::IncCompiler::new().compile_expr(expr)
 }
 
 pub fn compile_decl(decl: &AST)
                     -> Result<Decl, Error> {
-  let vec: Vec<&AST> = DottedExpr::new(decl).try_into()?;
-  if vec.is_empty() {
-    return Err(Error::InvalidDecl(decl.clone()));
-  }
-  match vec[0] {
-    AST::Symbol(s) => {
-      match s.borrow() {
-        "defn" => {
-          if vec.len() < 3 {
-            return Err(Error::InvalidDecl(decl.clone()));
-          }
-          let name = match vec[1] {
-            AST::Symbol(s) => s,
-            _ => return Err(Error::InvalidDecl(decl.clone())),
-          };
-          let args: Vec<_> = DottedExpr::new(vec[2]).try_into()?;
-          let args = ArgList::parse(args)?;
-          let body = vec[3..].iter().map(|expr| compile_expr(expr)).collect::<Result<Vec<_>, _>>()?;
-          Ok(Decl::FnDecl(decl::FnDecl {
-            name: name.to_owned(),
-            args: args,
-            body: Expr::Progn(body),
-          }))
-        }
-        "defmacro" => {
-          if vec.len() < 3 {
-            return Err(Error::InvalidDecl(decl.clone()));
-          }
-          let name = match vec[1] {
-            AST::Symbol(s) => s,
-            _ => return Err(Error::InvalidDecl(decl.clone())),
-          };
-          let args: Vec<_> = DottedExpr::new(vec[2]).try_into()?;
-          let args = ArgList::parse(args)?;
-          let body = vec[3..].iter().map(|expr| compile_expr(expr)).collect::<Result<Vec<_>, _>>()?;
-          Ok(Decl::MacroDecl(decl::MacroDecl {
-            name: name.to_owned(),
-            args: args,
-            body: Expr::Progn(body),
-          }))
-        }
-        _ => {
-          Err(Error::UnknownDecl(s.clone()))
-        }
-      }
-    }
-    _ => {
-      Err(Error::InvalidDecl(decl.clone()))
-    }
-  }
+  incremental::IncCompiler::new().compile_decl(decl)
 }
 
 pub fn compile_toplevel(body: &AST)
                         -> Result<Vec<Decl>, Error> {
-  let body: Vec<_> = DottedExpr::new(body).try_into()?;
-  let mut decls: Vec<Decl> = Vec::new();
-  let mut main: Vec<Expr> = Vec::new();
-  for curr in body {
-    match compile_decl(curr) {
-      Ok(d) => decls.push(d),
-      Err(Error::UnknownDecl(_)) => main.push(compile_expr(curr)?),
-      Err(e) => return Err(e),
-    }
-  }
-  let main_decl = Decl::FnDecl(decl::FnDecl {
-    name: MAIN_BODY_NAME.to_owned(),
-    args: ArgList::empty(),
-    body: Expr::Progn(main),
-  });
-  decls.push(main_decl);
-  Ok(decls)
+  let compiler = incremental::IncCompiler::new();
+  compiler.compile_toplevel(body)
 }
 
 // TODO For now, we can only call symbols. We'll need to extend this
@@ -164,6 +59,9 @@ fn resolve_special_form(head: &str,
 mod tests {
   use super::*;
   use crate::sxp::ast;
+  use crate::ir::literal::Literal;
+  use crate::ir::expr::Expr;
+  use crate::ir::arglist::ArgList;
 
   #[test]
   fn compile_call() {
