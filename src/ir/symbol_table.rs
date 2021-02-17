829bdd149e0d921d177272f49ba2fdcf59de8035
Silvio Mayolo <mercerenies@comcast.net>
2021-02-16 20:23:14 -0500

    Helper functions for symbol table

--- src/ir/symbol_table.rs
+++ src/ir/symbol_table.rs
@@ -32,12 +32,30 @@ impl SymbolTable {
     self.get(name).is_some()
   }
 
+  pub fn filter(&self, condition: impl FnMut(&Decl) -> bool) -> SymbolTable {
+    let vec = Vec::from(self.clone());
+    let filtered = vec.into_iter().filter(condition).collect::<Vec<_>>();
+    filtered.into()
+  }
+
 }
 
 impl From<SymbolTable> for Vec<Decl> {
 
   fn from(table: SymbolTable) -> Vec<Decl> {
     table.in_order
+  }
+
+}
+
+impl From<Vec<Decl>> for SymbolTable {
+
+  fn from(decls: Vec<Decl>) -> SymbolTable {
+    let mut table = SymbolTable::new();
+    for decl in decls {
+      table.set(decl.name().to_owned(), decl);
+    }
+    table
   }
 
 }
