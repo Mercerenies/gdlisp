b2874b15b405ac952e0b167c0c8cc5b0f85ac9e2
Silvio Mayolo <mercerenies@comcast.net>
2021-02-16 20:36:06 -0500

    Moved transitive_dependencies into Dependencies impl

--- src/ir/depends.rs
+++ src/ir/depends.rs
@@ -9,37 +9,41 @@ pub struct Dependencies {
   pub unknown: HashSet<String>,
 }
 
-pub fn transitive_dependencies(table: &SymbolTable, name: &str) -> Dependencies {
-  let mut visited = HashSet::new();
-  let mut unknown = HashSet::new();
-  match table.get(name) {
-    None => {
-      // No traversal necessary.
-      unknown.insert(name.to_owned());
-    }
-    Some(initial) => {
-      let mut frontier = vec!(initial);
-      while let Some(current) = frontier.pop() {
-        if visited.contains(current.name()) {
-          continue;
-        }
-        let deps = current.dependencies();
-        for dep in deps {
-          match table.get(&dep) {
+impl Dependencies {
+
+  pub fn identify(table: &SymbolTable, name: &str) -> Dependencies {
+    let mut visited = HashSet::new();
+    let mut unknown = HashSet::new();
+    match table.get(name) {
+      None => {
+        // No traversal necessary.
+        unknown.insert(name.to_owned());
+      }
+      Some(initial) => {
+        let mut frontier = vec!(initial);
+        while let Some(current) = frontier.pop() {
+          if visited.contains(current.name()) {
+            continue;
+          }
+          let deps = current.dependencies();
+          for dep in deps {
+            match table.get(&dep) {
             None => {
               unknown.insert(dep);
             }
-            Some(next) => {
-              frontier.push(next);
+              Some(next) => {
+                frontier.push(next);
+              }
             }
           }
-        }
-        visited.insert(current.name().to_owned());
+          visited.insert(current.name().to_owned());
+      }
       }
     }
+    Dependencies {
+      known: visited,
+      unknown: unknown,
+    }
   }
-  Dependencies {
-    known: visited,
-    unknown: unknown,
-  }
+
 }
