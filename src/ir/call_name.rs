4f8558be4711f36f344c1e27cdd44a4aba7ec509
Silvio Mayolo <mercerenies@comcast.net>
2021-02-20 21:12:10 -0500

    Minor style improvement (clippy)

--- src/ir/call_name.rs
+++ src/ir/call_name.rs
@@ -13,11 +13,9 @@ impl CallName {
 
   pub fn try_resolve_method_name(ast: &AST) -> Option<(&AST, &str)> {
     if let DottedExpr { elements: vec, terminal: AST::Nil } = DottedExpr::new(ast) {
-      if vec.len() == 3 {
-        if *vec[0] == ast::symbol("access-slot") {
-          if let AST::Symbol(name) = vec[2] {
-            return Some((vec[1], name));
-          }
+      if vec.len() == 3 && *vec[0] == ast::symbol("access-slot") {
+        if let AST::Symbol(name) = vec[2] {
+          return Some((vec[1], name));
         }
       }
     }
