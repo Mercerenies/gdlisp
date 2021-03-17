5610431f681d7b8b13ebbfd9979b85513ae045ad
Silvio Mayolo <mercerenies@comcast.net>
2021-03-16 22:22:52 -0400

    Noop tests

--- src/optimize/gdscript/noop.rs
+++ src/optimize/gdscript/noop.rs
@@ -34,3 +34,40 @@ pub fn is_expr_noop(expr: &Expr) -> bool {
     Expr::ArrayLit(v) => v.iter().all(is_expr_noop),
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::gdscript::op;
+
+  #[test]
+  fn expr_noop() {
+    // True
+    assert!(is_expr_noop(&Expr::var("example_variable")));
+    assert!(is_expr_noop(&Expr::from(1)));
+    assert!(is_expr_noop(&Expr::Unary(op::UnaryOp::Negate, Box::new(Expr::from(1)))));
+    assert!(is_expr_noop(&Expr::ArrayLit(vec!())));
+    assert!(is_expr_noop(&Expr::ArrayLit(vec!(Expr::from(1), Expr::from(2), Expr::from(3)))));
+
+    // False
+    let call = Expr::Call(None, String::from("function_name"), vec!());
+    assert!(!is_expr_noop(&Expr::Subscript(Box::new(Expr::from(1)), Box::new(Expr::from(2)))));
+    assert!(!is_expr_noop(&Expr::Attribute(Box::new(Expr::from(1)), String::from("attribute_name"))));
+    assert!(!is_expr_noop(&call));
+    assert!(!is_expr_noop(&Expr::Unary(op::UnaryOp::Negate, Box::new(call.clone()))));
+    assert!(!is_expr_noop(&Expr::Binary(Box::new(Expr::from(2)), op::BinaryOp::Add, Box::new(call.clone()))));
+  }
+
+  #[test]
+  fn stmt_noop() {
+    // True
+    assert!(is_code_noop(&Stmt::Expr(Expr::from(1))));
+    assert!(is_code_noop(&Stmt::PassStmt));
+    // False
+    let call = Expr::Call(None, String::from("function_name"), vec!());
+    assert!(!is_code_noop(&Stmt::Expr(call)));
+    assert!(!is_code_noop(&Stmt::BreakStmt));
+    assert!(!is_code_noop(&Stmt::ContinueStmt));
+  }
+
+}
