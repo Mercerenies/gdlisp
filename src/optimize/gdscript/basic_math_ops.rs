df04100cab7da085d2d7c455bd237ab01fa17992
Silvio Mayolo <mercerenies@comcast.net>
2021-03-29 19:55:15 -0400

    More BasicMathOps stuff

--- src/optimize/gdscript/basic_math_ops.rs
+++ src/optimize/gdscript/basic_math_ops.rs
@@ -1,20 +1,37 @@
 
 use super::ExpressionLevelPass;
-use crate::gdscript::expr::Expr;
+use crate::gdscript::expr::{self, Expr};
 use crate::gdscript::op;
 use crate::compile::error::Error;
+use super::constant;
 
 pub struct BasicMathOps;
 
 impl ExpressionLevelPass for BasicMathOps {
   fn run_on_expr(&self, expr: &Expr) -> Result<Expr, Error> {
-    // Currently, this only does one thing: eliminate double
-    // negations. (TODO Expand this)
+    let mut expr = expr.clone();
+    // (TODO Expand this)
 
-    // Double negation elimination
-    if let Expr::Unary(op::UnaryOp::Not, outer) = expr {
+    // Negation on constant
+    if let Expr::Unary(op::UnaryOp::Not, inner) = &expr {
+      if let Some(b) = constant::deduce_bool(inner) {
+        expr = Expr::from(!b);
+      }
+    }
+
+    // Ternary if on constant
+    if let Expr::TernaryIf(if_expr) = &expr {
+      let expr::TernaryIf { true_case, cond, false_case } = if_expr.clone();
+      if let Some(b) = constant::deduce_bool(&cond) {
+        expr = if b { *true_case } else { *false_case };
+      }
+    }
+
+    // Double negation elimination (TODO Maybe only in if statements;
+    // it does technically Booleanize at least)
+    if let Expr::Unary(op::UnaryOp::Not, outer) = &expr {
       if let Expr::Unary(op::UnaryOp::Not, inner) = &**outer {
-        return self.run_on_expr(inner);
+        expr = (**inner).clone();
       }
     }
 
