9b3709eebc9f4895fe80d415ff8fc7c0295da52c
Silvio Mayolo <mercerenies@comcast.net>
2021-07-22 15:02:19 -0400

    Docs

--- src/compile/stateful.rs
+++ src/compile/stateful.rs
@@ -1,4 +1,6 @@
 
+//! Types for expressing whether or not an [`Expr`] has side effects.
+
 use super::Compiler;
 use super::stmt_wrapper::{self, StmtWrapper};
 use super::body::builder::StmtBuilder;
@@ -6,15 +8,66 @@ use crate::gdscript::expr::Expr;
 use crate::gdscript::library;
 use crate::ir::locals::AccessType;
 
+/// An `StExpr` is an expression coupled with a declaration of that
+/// expression's side effects.
+///
+/// Note that such a declaration is context-sensitive, so there is no
+/// general-purpose function for converting an `Expr` into an
+/// `StExpr`. For example, simple variable access `foobar` could be
+/// read-only, assuming the variable is a local variable, but it could
+/// also be read-write, if the variable is an instance variable which
+/// has a `setget` modifier. Likewise, a function call is generally
+/// read-write, but if we can prove that the call is free of side
+/// effects, we can reduce this.
 #[derive(Debug, Clone)]
 pub struct StExpr {
   pub expr: Expr,
   pub side_effects: SideEffects
 }
 
+/// A declaration of side effects.
+///
+/// `SideEffects` has an [`Ord`] instance, where more invasive effects
+/// are considered greater than less invasive ones. This makes it
+/// possible to use [`std::cmp::max`] to take the more invasive of two
+/// side effects.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-pub enum SideEffects { None, ReadsState, ModifiesState }
+pub enum SideEffects {
+  /// The expression has no side effects. An expression with this
+  /// declaration modifies nothing and reads no mutable state. It
+  /// should be possible to move the expression, for inlining
+  /// purposes, to anywhere else within the same scope, or anywhere
+  /// else which contains the appropriate constant values necessary
+  /// for evaluation. An expression with `SideEffects::None` can
+  /// safely be evaluated several times, or have several identical
+  /// evaluations reduced to one. The optimizer has a great deal of
+  /// control over these expressions.
+  None,
+  /// The expression does not modify any state, but it may read from
+  /// mutable state. An expression with `SideEffects::ReadsState` may
+  /// be evaluated multiple times, or may have multiple evaluations
+  /// reduced to one, provided that no [`SideEffects::ModifiesState`]
+  /// calls happen in between the evaluations. Such expressions may
+  /// also be reordered relative to other `SideEffects::ReadsState` or
+  /// `SideEffects::None` expressions.
+  ReadsState,
+  /// The expression may read or write to ambient state. It is not
+  /// safe to reorder multiple `SideEffects::ModifiesState`
+  /// expressions relative to one another, and evaluating such a
+  /// statement multiple times is different than evaluating it only
+  /// once. The optimizer is very limited in this case.
+  ModifiesState,
+}
 
+/// A Boolean-isomorphic type indicating whether a result is required.
+///
+/// An argument of type `NeedsResult` is used in functions such as
+/// [`Compiler::compile_expr`] to indicate whether or not the result
+/// is going to be used for anything. There are some expressions that
+/// can compile to a more efficient form if we know the result is
+/// unneeded. For instance, `if` expressions in general need to create
+/// a local variable to store the result in, but if we know the result
+/// is going to be discarded, then we can safely skip that step.
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum NeedsResult { No, Yes }
 
@@ -32,14 +85,33 @@ impl From<bool> for NeedsResult {
 
 impl NeedsResult {
 
+  /// Returns `Yes` iff either `self` or `other` is `Yes`.
   pub fn or(self, other: NeedsResult) -> NeedsResult {
     NeedsResult::from(self == NeedsResult::Yes || other == NeedsResult::Yes)
   }
 
+  /// Returns `Yes` iff both `self` and `other` are `Yes`.
   pub fn and(self, other: NeedsResult) -> NeedsResult {
     NeedsResult::from(self == NeedsResult::Yes && other == NeedsResult::Yes)
   }
 
+  /// Construct an appropriate [`StmtWrapper`] that can store the
+  /// result of an expression, if needed.
+  ///
+  /// If `self` is `Yes`, then this function declares a variable (into
+  /// `builder`) with a local, generated name (using `prefix` as the
+  /// prefix) and then returns a `StmtWrapper` which assigns a value
+  /// to that variable. The `Expr`, in this case, is the variable
+  /// name.
+  ///
+  /// If `self` is `No`, then this function returns a vacuous
+  /// `StmtWrapper` and [`library::nil`] as the expression.
+  ///
+  /// In general, the returned `StmtWrapper` should be supplied with
+  /// the Godot expression which is, semantically, the result of the
+  /// IR expression, and the returned `Expr` can be used to reference
+  /// it later. If `self` is `No`, then the returned `Expr` is nil, as
+  /// we chose not to store the result anywhere.
   pub fn into_destination<'a>(self,
                               compiler: &mut Compiler<'a>,
                               builder: &mut StmtBuilder,
@@ -58,20 +130,43 @@ impl NeedsResult {
 }
 
 impl SideEffects {
+
+  /// Whether or not `self` is *at least* `SideEffects::ReadsState`.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// # use gdlisp::compile::stateful::SideEffects;
+  /// assert!(!SideEffects::None.reads_state());
+  /// assert!(SideEffects::ReadsState.reads_state());
+  /// assert!(SideEffects::ModifiesState.reads_state());
+  /// ```
   pub fn reads_state(&self) -> bool {
     *self >= SideEffects::ReadsState
   }
+
+  /// Whether or not `self` is *at least*
+  /// `SideEffects::ModifiesState`.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// # use gdlisp::compile::stateful::SideEffects;
+  /// assert!(!SideEffects::None.modifies_state());
+  /// assert!(!SideEffects::ReadsState.modifies_state());
+  /// assert!(SideEffects::ModifiesState.modifies_state());
+  /// ```
   pub fn modifies_state(&self) -> bool {
     *self >= SideEffects::ModifiesState
   }
 }
 
-// If we're writing to a variable, then the side effect is obviously
-// ModifiesState and this From instance should not be used. If we're
-// reading from a variable, then we can convert the AccessType of that
-// variable into a SideEffects by determining whether or not the
-// variable is ever modified (x.is_written_to) to figure out if we're
-// querying mutable state or simply accessing a constant value.
+/// If we're writing to a variable, then the side effect is obviously
+/// ModifiesState and this From instance should not be used. If we're
+/// reading from a variable, then we can convert the AccessType of
+/// that variable into a SideEffects by determining whether or not the
+/// variable is ever modified (x.is_written_to) to figure out if we're
+/// querying mutable state or simply accessing a constant value.
 impl From<AccessType> for SideEffects {
   fn from(x: AccessType) -> SideEffects {
     if x.is_written_to() {
--- src/compile/stmt_wrapper.rs
+++ src/compile/stmt_wrapper.rs
@@ -1,29 +1,50 @@
 
+//! Trait and implementations for conveniently wrapping [`Expr`] into
+//! [`Stmt`].
+//!
+//! This module defines the [`StmtWrapper`] trait, as well as several
+//! implementations of it by structs.
+
 use crate::gdscript::stmt::Stmt;
 use crate::gdscript::expr::Expr;
 use crate::gdscript::op;
 use crate::compile::body::builder::StmtBuilder;
 use super::StExpr;
 
+/// A `StmtWrapper` provides a mechanism for taking an arbitrary
+/// [`Expr`] and wrapping it into a [`Stmt`]. It can be thought of as
+/// a `Stmt` with a hole in it, which we'll later fill with an
+/// expression.
 pub trait StmtWrapper {
 
-  // The wrapping behavior for the statement wrapper is performed
-  // here.
+  /// Wraps the expression in a statement.
   fn wrap_expr(&self, expr: Expr) -> Stmt;
 
-  // Normally, a wrapper, as the name implies, wraps an expression in
-  // a statement. Sometimes, however, the expression has no side
-  // effects, and the work itself has already been done elsewhere. In
-  // this case, is_vacuous specifies whether we actually *need* the
-  // result or not. If is_vacuous is true and the expression has no
-  // side effects, it will be elided completely.
+  /// Normally, a wrapper, as the name implies, wraps an expression in
+  /// a statement. Sometimes, however, the expression has no side
+  /// effects, and the work itself has already been done elsewhere. In
+  /// this case, `is_vacuous` specifies whether we actually *need* the
+  /// result or not. If `is_vacuous` is true and the expression has no
+  /// side effects, it will be elided completely.
+  ///
+  /// The default `is_vacuous` always returns false. It can be
+  /// overridden in implementors.
   fn is_vacuous(&self) -> bool {
     false
   }
 
-  // Wraps the expression and places it in the statement builder. If
-  // the wrapper is vacuous and the expression is stateless, this
-  // method does nothing.
+  /// Wraps `expr` and places it in `builder`.
+  ///
+  /// If `self` is vacuous and `expr` does not modify state, this
+  /// method does nothing. The reasoning for this is as follows. If
+  /// `expr.side_effects` does not modify state, then the expression,
+  /// as a value, is only being used for its result, not for its side
+  /// effects. If `self.is_vacuous()` is true, then we've declared
+  /// that this statement wrapper does not care about the result. So
+  /// if we have an expression for which the only thing that matters
+  /// is the result and a statement wrapper that's ready to ignore
+  /// that result, then we effectively have no need to ever evaluate
+  /// the expression in the first place.
   fn wrap_to_builder(&self, builder: &mut StmtBuilder, expr: StExpr) {
     let StExpr { expr, side_effects } = expr;
     if side_effects.modifies_state() || !self.is_vacuous() {
@@ -31,6 +52,11 @@ pub trait StmtWrapper {
     }
   }
 
+  /// Wraps `expr` and returns either zero or one statements, using
+  /// the same reasoning at [`StmtWrapper.wrap_to_builder`]. If `self`
+  /// is vacuous and the expression is stateless, then the vector will
+  /// be empty. Otherwise, a single statement will be produced via
+  /// [`StmtWrapper::wrap_expr`].
   fn wrap_to_stmts(&self, expr: StExpr) -> Vec<Stmt> {
     let StExpr { expr, side_effects } = expr;
     if side_effects.modifies_state() || !self.is_vacuous() {
@@ -42,8 +68,18 @@ pub trait StmtWrapper {
 
 }
 
+/// A [`StmtWrapper`] which wraps the expression in a
+/// [`Stmt::ReturnStmt`].
 pub struct Return;
+
+/// A [`StmtWrapper`] which wraps the expression in a [`Stmt::Expr`].
+/// This is a vacuous statement wrapper, as per
+/// [`StmtWrapper::is_vacuous`].
 pub struct Vacuous;
+
+/// A [`StmtWrapper`] which wraps the expression in a
+/// [`Stmt::Assign`], where the left-hand side is given by the
+/// `AssignToExpr` value.
 pub struct AssignToExpr(pub Expr);
 
 impl StmtWrapper for Return {
@@ -64,6 +100,8 @@ impl StmtWrapper for Vacuous {
 
 }
 
+/// An [`AssignToExpr`] which assigns to an [`Expr::Var`] with the
+/// given name.
 pub fn assign_to_var(s: String) -> AssignToExpr {
   AssignToExpr(Expr::Var(s))
 }
