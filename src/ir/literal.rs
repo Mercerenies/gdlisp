28d64dc7e20cef7a1297f5ec7d38185d5bdfb1b8
Silvio Mayolo <mercerenies@comcast.net>
2021-07-22 23:33:07 -0400

    Some From instances for Literal

--- src/ir/literal.rs
+++ src/ir/literal.rs
@@ -1,6 +1,9 @@
 
+//! GDLisp literal values.
+
 use ordered_float::OrderedFloat;
 
+/// A GDLisp literal value.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Literal {
   Nil,
@@ -9,4 +12,34 @@ pub enum Literal {
   String(String),
   Symbol(String),
   Bool(bool),
+}
+
+impl From<i32> for Literal {
+  fn from(value: i32) -> Literal {
+    Literal::Int(value)
+  }
+}
+
+impl From<OrderedFloat<f32>> for Literal {
+  fn from(value: OrderedFloat<f32>) -> Literal {
+    Literal::Float(value)
+  }
+}
+
+impl From<String> for Literal {
+  fn from(value: String) -> Literal {
+    Literal::String(value)
+  }
+}
+
+impl<'a> From<&'a str> for Literal {
+  fn from(value: &'a str) -> Literal {
+    Literal::String(String::from(value))
+  }
+}
+
+impl From<bool> for Literal {
+  fn from(value: bool) -> Literal {
+    Literal::Bool(value)
+  }
 }
