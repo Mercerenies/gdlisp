64f3f177ffe11e6fd13646073308896b3371b75b
Silvio Mayolo <mercerenies@comcast.net>
2022-02-22 16:07:52 -0500

    DebugWrapper serde

--- src/util/debug_wrapper.rs
+++ src/util/debug_wrapper.rs
@@ -2,6 +2,8 @@
 //! Wrapper for non-[`Debug`] types that prints a trivial `Debug`
 //! representation.
 
+use serde::{Serialize, Deserialize};
+
 use std::fmt::{self, Debug};
 
 /// A `DebugWrapper` is a very thin wrapper around a `T`.
@@ -9,7 +11,7 @@ use std::fmt::{self, Debug};
 /// unconditionally implements [`Debug`] for all types. The debug
 /// representation for every instance of `DebugWrapper` is always an
 /// ellipsis `"..."`.
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
 #[repr(transparent)]
 pub struct DebugWrapper<T>(pub T);
 
