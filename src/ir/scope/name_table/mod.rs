853aa4916b772b305a55ebc893ffe85caab6a753
Silvio Mayolo <mercerenies@comcast.net>
2022-03-19 13:56:53 -0400

    map_ns

--- src/ir/scope/name_table/mod.rs
+++ src/ir/scope/name_table/mod.rs
@@ -46,6 +46,24 @@ impl<NS: Hash + Eq + Clone> NameTable<NS> {
     self.data.len()
   }
 
+  /// Maps the namespace type of this name table, producing a new name
+  /// table where the namespace of every known name has been
+  /// transformed by the given function.
+  ///
+  /// This is mainly intended to be used with injective functions. If
+  /// the function argument is injective, then the new name table will
+  /// have no loss of information and will contain exactly as many
+  /// names as the original. If the function is not injective, then it
+  /// is unspecified which name will win out in the case of collisions
+  /// in the resulting name table.
+  pub fn map_ns<NS1: Hash + Eq + Clone>(self, mut function: impl FnMut(NS) -> NS1) -> NameTable<NS1> {
+    let mut result: NameTable<NS1> = NameTable::new();
+    for ((ns, name), pos) in self.data {
+      result.add_name(function(ns), name, pos);
+    }
+    result
+  }
+
 }
 
 impl<NS: Hash + Eq + Clone> Default for NameTable<NS> {
@@ -59,7 +77,7 @@ impl<NS: Hash + Eq + Clone> Default for NameTable<NS> {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::ir::identifier::Namespace;
+  use crate::ir::identifier::{Namespace, ClassNamespace};
 
   #[test]
   fn empty_table() {
@@ -101,6 +119,37 @@ mod tests {
     assert_eq!(table.add_name(Namespace::Value, String::from("NAME"), SourceOffset(20)), None);
     assert_eq!(table.add_name(Namespace::Value, String::from("NAME"), SourceOffset(10)), Some(SourceOffset(20)));
     assert_eq!(table.len(), 1);
+  }
+
+  #[test]
+  fn table_map_ns() {
+
+    let mut original_table: NameTable<Namespace> = NameTable::new();
+    assert_eq!(original_table.add_name(Namespace::Value, String::from("VALUE"), SourceOffset(20)), None);
+    assert_eq!(original_table.add_name(Namespace::Function, String::from("FUNCTION"), SourceOffset(10)), None);
+
+    let mut new_table: NameTable<ClassNamespace> = NameTable::new();
+    assert_eq!(new_table.add_name(ClassNamespace::Value, String::from("VALUE"), SourceOffset(20)), None);
+    assert_eq!(new_table.add_name(ClassNamespace::Function, String::from("FUNCTION"), SourceOffset(10)), None);
+
+    let mapped_table = original_table.map_ns(ClassNamespace::from);
+    assert_eq!(mapped_table, new_table);
+
+  }
+
+  #[test]
+  fn table_map_ns_collision() {
+
+    let mut original_table: NameTable<Namespace> = NameTable::new();
+    assert_eq!(original_table.add_name(Namespace::Value, String::from("A"), SourceOffset(20)), None);
+    assert_eq!(original_table.add_name(Namespace::Function, String::from("A"), SourceOffset(10)), None);
+
+    let mapped_table = original_table.map_ns(|_| Namespace::Value); // Cause the two names to collide.
+
+    // It is unspecified which name will win out, but there should
+    // only be one name in the resulting table.
+    assert_eq!(mapped_table.len(), 1);
+
   }
 
 }
