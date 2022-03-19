2b44fb73498798b01c3ac1d8e865d769a90adb1e
Silvio Mayolo <mercerenies@comcast.net>
2022-03-19 15:04:08 -0400

    Convenience helper

--- src/ir/scope/mod.rs
+++ src/ir/scope/mod.rs
@@ -5,3 +5,16 @@
 pub mod decl;
 pub mod name_table;
 pub mod error;
+
+use crate::ir::decl::TopLevel;
+use crate::compile::error::Error;
+
+/// Convenience method to perform all scope checking on an IR file
+/// representation.
+///
+/// Currently, this simply delegates to
+/// [`decl::check_all_decl_scopes`] and broadens the error type, but
+/// it will be expanded in the future to perform more detailed checks.
+pub fn check_scopes(toplevel: &TopLevel) -> Result<(),  Error> {
+  decl::check_all_decl_scopes(toplevel).map_err(Error::from)
+}
