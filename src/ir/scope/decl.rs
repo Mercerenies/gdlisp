501288bec31b08009d70b84cb62336aa6031749e
Silvio Mayolo <mercerenies@comcast.net>
2022-03-19 14:55:10 -0400

    check_all_decl_scopes

--- src/ir/scope/decl.rs
+++ src/ir/scope/decl.rs
@@ -8,10 +8,13 @@
 use super::name_table::NameTable;
 use super::name_table::builder::NameTableBuilder;
 use super::error::ScopeError;
+use crate::pipeline::source::SourceOffset;
 use crate::ir::identifier::{Namespace, ClassNamespace};
-use crate::ir::decl::{TopLevel, ClassDecl};
-use crate::ir::expr::LambdaClass;
+use crate::ir::decl::{DeclF, TopLevel, ClassDecl};
+use crate::ir::expr::{Expr, ExprF, LambdaClass};
+use crate::ir::literal::Literal;
 use crate::gdscript::library;
+use crate::optimize::ir::expr_walker::walk_exprs_in_toplevel_ok;
 
 use std::hash::Hash;
 
@@ -108,6 +111,64 @@ impl DeclScope<ClassNamespace> for LambdaClass {
     Ok(builder.build())
   }
 
+}
+
+impl<'a, T, NS: Hash + Eq + Clone> DeclScope<NS> for &'a T
+where T: DeclScope<NS> {
+  fn get_scope_names(&self) -> Result<NameTable<NS>, ScopeError<NS>> {
+    (*self).get_scope_names()
+  }
+}
+
+/// Return a vector of all of the declaration scopes in the given
+/// file, in an unspecified order.
+///
+/// This function returns all of the following.
+/// * The toplevel scope itself.
+/// * Any class scopes introduced in the file, including the main class, which
+///   is considered distinct from the toplevel.
+/// * Any anonymous classes defined in the field.
+pub fn get_all_decl_scopes<'a>(toplevel: &'a TopLevel) -> Vec<Box<dyn DeclScope<ClassNamespace> + 'a>> {
+  let mut acc: Vec<Box<dyn DeclScope<ClassNamespace> + 'a>> = Vec::new();
+
+  // The toplevel scope
+  acc.push(Box::new(ClassNamespaceAdaptor(toplevel)));
+
+  // Any classes defined in the toplevel
+  for decl in &toplevel.decls {
+    match &decl.value {
+      DeclF::ClassDecl(cls) => {
+        acc.push(Box::new(cls));
+      }
+      DeclF::FnDecl(_) | DeclF::MacroDecl(_) | DeclF::SymbolMacroDecl(_) | DeclF::ConstDecl(_) | DeclF::EnumDecl(_) | DeclF::DeclareDecl(_) => {}
+    }
+  }
+
+  // Any anonymous classes declared in the file
+  walk_exprs_in_toplevel_ok(toplevel, |expr| {
+    if let ExprF::LambdaClass(cls) = &expr.value {
+      // *sigh* What an unfortunate copy. But I don't see a way to
+      // convince the borrow checker that this value isn't going to
+      // disappear. (TODO Yeah...)
+      acc.push(cls.clone());
+    }
+    // Note: We don't use this value, so if this string ever appears
+    // in the output code, there's a problem.
+    Expr::literal(Literal::from("UNUSED STRING FROM get_all_decl_scopes"), SourceOffset(0))
+  });
+
+  acc
+}
+
+/// Given a file, run the check for duplicate names, returning an
+/// error if any are found. If no errors occur, returns normally.
+pub fn check_all_decl_scopes(toplevel: &TopLevel) -> Result<(), ScopeError<ClassNamespace>> {
+  for scope in get_all_decl_scopes(toplevel) {
+    // We don't need the resulting table; just any errors that occur
+    // while trying to produce it.
+    let _ = scope.get_scope_names()?;
+  }
+  Ok(())
 }
 
 // TODO Tests
