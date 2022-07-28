6a879417c11722111bf06954b0732ff1dbdd57e6
Silvio Mayolo <mercerenies@comcast.net>
2022-07-28 10:51:51 -0400

    api_type

--- src/gdscript/library/gdnative/api_type.rs
+++ src/gdscript/library/gdnative/api_type.rs
@@ -2,21 +2,34 @@
 //! The [`ApiType`] enum, indicating whether a GDScript class is in
 //! the core library or intended for editor tooling.
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
 pub enum ApiType {
   Core,
   Tools,
 }
 
-impl ApiType {
-  pub const CORE: &'static str = "core";
-  pub const TOOLS: &'static str = "tools";
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use serde_json;
 
-  pub fn as_string(&self) -> &'static str {
-    match self {
-      ApiType::Core => ApiType::CORE,
-      ApiType::Tools => ApiType::TOOLS,
-    }
+  #[test]
+  fn serialize_api_type_test() {
+    let core = serde_json::to_string(&ApiType::Core).unwrap();
+    assert_eq!(core, "\"core\"");
+    let tools = serde_json::to_string(&ApiType::Tools).unwrap();
+    assert_eq!(tools, "\"tools\"");
+  }
+
+  #[test]
+  fn deserialize_api_type_test() {
+    let core: ApiType = serde_json::from_str("\"core\"").unwrap();
+    assert_eq!(core, ApiType::Core);
+    let tools: ApiType = serde_json::from_str("\"tools\"").unwrap();
+    assert_eq!(tools, ApiType::Tools);
   }
 
 }
