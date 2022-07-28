270102dbc5faf8641bd20b204e145c2f2e7cc8b2
Silvio Mayolo <mercerenies@comcast.net>
2022-07-28 11:34:14 -0400

    Test for roundtrip argument

--- src/gdscript/library/gdnative/argument.rs
+++ src/gdscript/library/gdnative/argument.rs
@@ -9,3 +9,22 @@ pub struct Argument {
   pub has_default_value: bool,
   pub default_value: String,
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use serde_json;
+
+  #[test]
+  fn serialize_roundtrip_argument_test() {
+    let argument = Argument {
+      name: String::from("mesh"),
+      argument_type: String::from("Mesh"),
+      has_default_value: false,
+      default_value: String::from(""),
+    };
+    let serialized = serde_json::to_string(&argument).unwrap();
+    assert_eq!(serde_json::from_str::<Argument>(&serialized).unwrap(), argument);
+  }
+
+}
