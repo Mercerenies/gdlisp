9e85a6d9127ed89edc91ee120f8ed827fa992f40
Silvio Mayolo <mercerenies@comcast.net>
2022-07-29 15:13:42 -0400

    Valid identifier rules

--- src/gdscript/mod.rs
+++ src/gdscript/mod.rs
@@ -14,6 +14,8 @@ pub mod expr_wrapper;
 pub mod inner_class;
 pub mod metadata;
 
+use regex::Regex;
+
 use std::fmt;
 use std::convert::TryInto;
 
@@ -30,4 +32,99 @@ use std::convert::TryInto;
 pub fn indent<W : fmt::Write>(w : &mut W, ind: u32) -> Result<(), fmt::Error> {
   let spaces = String::from(" ").repeat(ind.try_into().unwrap());
   write!(w, "{}", spaces)
+}
+
+/// Determines whether the string is a valid GDScript identifier, by
+/// the rules of the language. A GDScript identifier consists only of
+/// ASCII alphanumeric characters and underscore and cannot begin with
+/// an underscore.
+pub fn is_valid_identifier(s: &str) -> bool {
+  lazy_static! {
+    static ref RE: Regex = Regex::new(r#"\A[A-Za-z_][A-Za-z0-9_]*\z"#).unwrap();
+  }
+  RE.is_match(s)
+}
+
+/// Determines whether the string is a valid GDScript node path. A
+/// valid GDScript node path is a sequence of one or more valid
+/// identifiers, delimited by forward slashes.
+///
+/// Note: This function checks the *path* portion of the node path
+/// syntax, so a string containing a `$` will not be accepted.
+/// Additionally, this function is not designed to check the extended
+/// quoted node path syntax, so a string containing `"` will not be
+/// accepted.
+pub fn is_valid_node_path(s: &str) -> bool {
+  s.split('/').all(is_valid_identifier)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  fn do_indent(ind: u32) -> String {
+    let mut x = String::new();
+    indent(&mut x, ind).unwrap();
+    x
+  }
+
+  #[test]
+  fn test_indent() {
+    assert_eq!(do_indent(0), "");
+    assert_eq!(do_indent(1), " ");
+    assert_eq!(do_indent(2), "  ");
+    assert_eq!(do_indent(3), "   ");
+    assert_eq!(do_indent(4), "    ");
+  }
+
+  #[test]
+  fn test_identifier() {
+
+    // Valid
+    assert_eq!(is_valid_identifier("foo"), true);
+    assert_eq!(is_valid_identifier("foo99"), true);
+    assert_eq!(is_valid_identifier("FOO99"), true);
+    assert_eq!(is_valid_identifier("_"), true);
+    assert_eq!(is_valid_identifier("_a_b_c"), true);
+    assert_eq!(is_valid_identifier("_00_00_"), true);
+
+    // Invalid
+    assert_eq!(is_valid_identifier(""), false);
+    assert_eq!(is_valid_identifier("0"), false);
+    assert_eq!(is_valid_identifier("0.0"), false);
+    assert_eq!(is_valid_identifier("a.b"), false);
+    assert_eq!(is_valid_identifier("c/d"), false);
+    assert_eq!(is_valid_identifier("-"), false);
+
+  }
+
+  #[test]
+  fn test_path() {
+
+    // Valid
+    assert_eq!(is_valid_node_path("foo"), true);
+    assert_eq!(is_valid_node_path("foo99"), true);
+    assert_eq!(is_valid_node_path("_"), true);
+    assert_eq!(is_valid_node_path("_a_b_c"), true);
+    assert_eq!(is_valid_node_path("_00_00_"), true);
+    assert_eq!(is_valid_node_path("a/b"), true);
+    assert_eq!(is_valid_node_path("a/b/cdef"), true);
+    assert_eq!(is_valid_node_path("A/B/C"), true);
+
+    // Invalid
+    assert_eq!(is_valid_node_path(""), false);
+    assert_eq!(is_valid_node_path("0"), false);
+    assert_eq!(is_valid_node_path("0.0"), false);
+    assert_eq!(is_valid_node_path("a.b"), false);
+    assert_eq!(is_valid_node_path("c//d"), false);
+    assert_eq!(is_valid_node_path("/a"), false);
+    assert_eq!(is_valid_node_path("a/"), false);
+    assert_eq!(is_valid_node_path("/"), false);
+    assert_eq!(is_valid_node_path("/0"), false);
+    assert_eq!(is_valid_node_path("0/"), false);
+    assert_eq!(is_valid_node_path("foo/bar/0"), false);
+    assert_eq!(is_valid_node_path("-"), false);
+
+  }
+
 }
