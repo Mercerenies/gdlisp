dd62dcd012d2295435f71756d1405870c3822dd4
Silvio Mayolo <mercerenies@comcast.net>
2022-08-05 20:23:50 -0400

    ConstructorArgList

--- src/ir/arglist.rs
+++ src/ir/arglist.rs
@@ -44,6 +44,17 @@ pub struct SimpleArgList {
   pub args: Vec<String>,
 }
 
+/// A constructor argument list consists of only required arguments,
+/// similar to [`SimpleArgList`]. However, some of the arguments can
+/// be marked as instance fields. These will be assigned to the
+/// corresponding instance variable on the class at runtime.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ConstructorArgList {
+  /// The list of required arguments, together with whether or not
+  /// they've been marked as instance variables.
+  pub args: Vec<(String, bool)>,
+}
+
 /// The type of "rest" argument which accumulates any extra arguments
 /// to a function call.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -344,6 +355,60 @@ impl SimpleArgList {
     ArgList::parse(args).and_then(|arglist| {
       SimpleArgList::try_from(arglist).map_err(|err| ArgListParseError::new(err, pos))
     })
+  }
+
+  /// The length of the argument list.
+  pub fn len(&self) -> usize {
+    self.args.len()
+  }
+
+  /// Whether the argument list consists of zero arguments.
+  pub fn is_empty(&self) -> bool {
+    self.args.is_empty()
+  }
+
+}
+
+impl ConstructorArgList {
+
+  /// Converts the argument list into a GDScript argument list, using
+  /// the given name generator to produce unique names, similar to
+  /// [`ArgList::into_gd_arglist`].
+  ///
+  /// The second return value of this function includes, in addition
+  /// to the name translations, whether or not each argument is marked
+  /// as a constructor instance field.
+  pub fn into_gd_arglist(self, gen: &mut impl NameGenerator) -> (GDArgList, Vec<(NameTrans, bool)>) {
+    let (args, instance_fields): (Vec<_>, Vec<_>) = self.args.into_iter().unzip();
+    let arglist = SimpleArgList { args };
+    let (gd_arglist, trans) = arglist.into_gd_arglist(gen);
+    let names: Vec<(NameTrans, bool)> = trans.into_iter().zip(instance_fields.into_iter()).collect();
+    (gd_arglist, names)
+  }
+
+  /// An iterator over all variable names, together with whether or
+  /// not they are instance variables, mentioned in the argument list,
+  /// in order.
+  pub fn iter_vars(&self) -> impl Iterator<Item = (&str, bool)> {
+    self.args.iter().map(|(name, field)| (name.borrow(), *field))
+  }
+
+  /// Attempts to parse a constructor argument list from the iterator.
+  /// A constructor argument list can contain ordinary names, just
+  /// like a [`SimpleArgList`], but those names can also be written in
+  /// one of the following three equivalent forms.
+  ///
+  /// ```text
+  /// @foobar
+  /// self:foobar
+  /// (access-slot self foobar)
+  /// ```
+  ///
+  /// In that case, the argument will be marked as a constructor
+  /// instance field argument.
+  pub fn parse<'a>(args: impl IntoIterator<Item = &'a AST>, pos: SourceOffset)
+                   -> Result<ConstructorArgList, ArgListParseError> {
+    todo!() /////
   }
 
   /// The length of the argument list.
