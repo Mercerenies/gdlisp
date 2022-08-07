36f44337c17208453f9d4c545c52563a1816348f
Silvio Mayolo <mercerenies@comcast.net>
2022-08-07 13:34:43 -0400

    Updated use of new API

--- src/gdscript/library/constant_loader.rs
+++ src/gdscript/library/constant_loader.rs
@@ -1,9 +1,9 @@
 
-use super::gdnative::{NativeClasses, GLOBAL_CONSTANTS_CLASS};
+use super::gdnative::NativeClasses;
 
 use std::borrow::Borrow;
 
 pub fn get_all_constants(classes: &NativeClasses) -> impl Iterator<Item=&str> {
-  let global_constants = classes.get(GLOBAL_CONSTANTS_CLASS).expect("Could not read global constants class from api.json");
+  let global_constants = classes.get_global_constants();
   global_constants.constants.keys().map(String::borrow)
 }
