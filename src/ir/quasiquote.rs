5913fd9b6680724e0fdf07c6d01e07a13cb0e0fe
Silvio Mayolo <mercerenies@comcast.net>
2022-10-25 20:52:12 -0400

    Docs

--- src/ir/quasiquote.rs
+++ src/ir/quasiquote.rs
@@ -8,34 +8,91 @@ use crate::pipeline::Pipeline;
 
 // TODO Where do I pick up PError in this file? Can we do all of this in GDError?
 
+/// A `QQSpliced` represents an expression within an `unquote` or
+/// `unquote-spliced` block. Specifically, a `QQSpliced` contains an
+/// expression that, when the quasiquote is evaluated, will have its
+/// value interpolated in some form or another into the surrounding
+/// expression.
 #[derive(Clone, Debug, PartialEq, Eq)]
 enum QQSpliced {
+  /// An expression that will be interpreted as a single atomic value.
+  /// The inside of an `unquote` block is parsed as this variant.
   Single(Expr),
+  /// An expression that will be spliced into an array or list
+  /// expression. The inside of an `unquote-spliced` block parses as
+  /// this variant.
+  ///
+  /// It is an error if a `QQSpliced::Several` appears in a context
+  /// other than a list or array, as it cannot be spliced. For
+  /// example, it is an error if a `QQSpliced::Several` appears within
+  /// a dictionary literal.
   Several(Expr),
 }
 
+/// A value within a quasiquoting expression. `UnquotedValue` always
+/// wraps a single [`AST`] and keeps track of what sort of expression
+/// it is, as far as the quasiquoting engine is concerned.
 #[derive(Clone, Debug, PartialEq, Eq)]
 enum UnquotedValue<'a> {
+  /// A simple, literal value which is not anything special as far as
+  /// the quasiquoting engine is concerned.
   SimpleValue(&'a AST),
+  /// A nested `quasiquote` block. The [`AST`] is the *inside* of the
+  /// `quasiquote`, excluding the block itself.
   Quasiquote(&'a AST),
+  /// An `unquote` block. The [`AST`] is the *inside* of the
+  /// `unquote`, excluding the block itself.
   Unquote(&'a AST),
+  /// An `unquote-spliced` block. The [`AST`] is the *inside* of the
+  /// `unquote-spliced`, excluding the block itself.
   UnquoteSpliced(&'a AST),
 }
 
 impl QQSpliced {
+
+  /// Extracts the [`QQSpliced::Single`] value from `self`. If `self`
+  /// is not a [`QQSpliced::Single`] then an error is returned instead.
+  ///
+  /// The [`AST`] argument should be the original S-expression from
+  /// which the `QQSpliced` was produced. It will only be used in the
+  /// case of an error in order to provide a more detailed diagnostic.
   fn into_single(self, ast: &AST) -> Result<Expr, GDError> {
     match self {
       QQSpliced::Single(e) => Ok(e),
       QQSpliced::Several(_) => Err(GDError::new(GDErrorF::BadUnquoteSpliced(ast.clone()), ast.pos)),
     }
   }
+
 }
 
 impl<'a> UnquotedValue<'a> {
+
+  /// Convenience constructor, delegates to
+  /// [`UnquotedValue::SimpleValue`].
   fn verbatim(arg: &'a AST) -> UnquotedValue<'a> {
     UnquotedValue::SimpleValue(arg)
   }
 
+  /// Reads the top-level S-expressions and looks for any special
+  /// quasiquote keywords. Specifically, if the S-expression is a
+  /// proper list of two elements and the first element is one of the
+  /// following symbols, then the function returns an appropriate
+  /// value.
+  ///
+  /// * If the symbol is `quasiquote`, then an
+  /// [`UnquotedValue::Quasiquote`] containing the second element of
+  /// the list is returned.
+  ///
+  /// * If the symbol is `unquote`, then an [`UnquotedValue::Unquote`]
+  /// containing the second element of the list is returned.
+  ///
+  /// * If the symbol is `unquote-spliced`, then an
+  /// [`UnquotedValue::UnquoteSpliced`] containing the second element
+  /// of the list is returned.
+  ///
+  /// If the symbol is anything else, or if the AST has any other
+  /// shape, then this function returns an
+  /// [`UnquotedValue::SimpleValue`] containing the entire AST.
   fn parse(arg: &'a AST) -> UnquotedValue<'a> {
     if let ASTF::Cons(car, cdr) = &arg.value {
       if let Some(name) = car.as_symbol_ref() {
@@ -73,6 +130,15 @@ pub fn quasiquote_with_depth(icompiler: &mut IncCompiler,
   engine.quasiquote_indexed(arg, 0, 0)
 }
 
+/// The quasiquoting engine is the internal class whose implementation
+/// recursively implements quasiquote parsing on an [`AST`].
+///
+/// A quasiquote engine requires mutable access to an [`IncCompiler`]
+/// and a [`Pipeline`], in order to call [`IncCompiler::compile_expr`]
+/// on unquoted values. Additionally, a quasiquote engine keeps track
+/// of how deep it is into a Godot expression. This is a workaround
+/// for a Godot parsing issue that manifests when an expression is
+/// nested too deeply.
 struct QuasiquoteEngine<'a, 'b> {
   icompiler: &'a mut IncCompiler,
   pipeline: &'b mut Pipeline,
@@ -81,6 +147,15 @@ struct QuasiquoteEngine<'a, 'b> {
 
 impl<'a, 'b> QuasiquoteEngine<'a, 'b> {
 
+  /// Given an incremental compiler and a pipeline, produce a
+  /// [`QuasiquoteEngine`].
+  ///
+  /// The `max_depth` argument indicates how deeply nested a generated
+  /// expression can be before the engine will split it into multiple
+  /// expressions with a temporary variable in between. For the
+  /// rationale behind this argument (as well as a reasonable default
+  /// value for it), see
+  /// [`MAX_QUOTE_REIFY_DEPTH`](crate::compile::frame::MAX_QUOTE_REIFY_DEPTH).
   fn new(icompiler: &'a mut IncCompiler, pipeline: &'b mut Pipeline, max_depth: u32) -> Self {
     QuasiquoteEngine { icompiler, pipeline, max_depth }
   }
